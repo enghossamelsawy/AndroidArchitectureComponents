d60da5f9ce4b0957a4e3b236111359285618e4e4
engho <enghossamelsawy@gmail.com>
2020-05-27 16:51:08 +0300

    add dependency for kotlin  gradle

---
+++ buildSrc/Src/main/java/constants/TestLibraryDependency.kt
@@ -0,0 +1,16 @@
+import TestLibraryDependency.Version.MOCKK
+
+object TestLibraryDependency {
+    object Version {
+        const val JUNIT = "4.13"
+        const val JUNIT_ANDROID = "1.1.1"
+        const val ESPRESSO = "3.2.0"
+        const val MOCKK = "1.10.0"
+    }
+
+    const val JUNIT = "junit:junit:${Version.JUNIT}"
+    const val JUNIT_ANDROID = "androidx.test.ext:junit:${Version.JUNIT_ANDROID}"
+    const val ESPRESSO = "androidx.test.espresso:espresso-core:${Version.ESPRESSO}"
+    const val KOIN = "org.koin:koin-test:${LibraryDependency.Version.KOIN}"
+    const val MOKK = "io.mockk:mockk:${MOCKK}"
+}
