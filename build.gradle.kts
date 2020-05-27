2de8db6cc8de1f8ad811f57c8c178fc70ed396b6
engho <enghossamelsawy@gmail.com>
2020-05-27 17:13:36 +0300

    fix issue in gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,8 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.6.3")
-        classpath(kotlin(GradleClasspath.KOTLIN_PlUGIN, version = CoreVersion.KOTLIN))
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
